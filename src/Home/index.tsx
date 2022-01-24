9c0d52a65be9d0b9744f0a29b0c38d9f064a508d
jiahao_yu <495913582@qq.com>
2022-01-24 18:06:26 +0800

    init

---
+++ src/Home/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import s from "./index.module.scss";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className={s.a}>
+                abc
+                <div className={s.b}>123</div>
+            </div>
+        );
+    }
+}
